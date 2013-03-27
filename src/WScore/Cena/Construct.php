17ca577a75f0efa586e31de4a4790a92a087927f
asao Kamei <asao@ubuPC.(none)>
2013-03-27 12:57:17 +0900

    created Construct for dealing with Cena formatting.

---
+++ src/WScore/Cena/Construct.php
@@ -0,0 +1,74 @@
+<?php
+namespace WScore\Cena;
+
+use WScore\DataMapper\Entity\EntityInterface;
+
+/**
+ * Class CenaManager
+ * @package WScore\Cena
+ *
+ * @singleton
+ */
+class Construct
+{
+    public $cena = 'Cena';
+
+    public $connector = '.';
+
+    /**
+     * de-compose cenaID into model, type, and id.
+     * example: decompose( 'Cena.model.type.id' ) will return [ 'model', 'type', 'id' ].
+     *
+     * @param $cenaId
+     * @return array
+     */
+    public function decompose( $cenaId )
+    {
+        $list = explode( $this->connector, $cenaId );
+        if( $list[0] == $this->cena ) array_shift( $list );
+        return $list;
+    }
+
+    /**
+     * returns cena-formatted name for form elements.
+     *
+     * @param string  $cenaId
+     * @param string  $type
+     * @param null    $name
+     * @return string
+     */
+    public function composeFormName( $cenaId, $type='prop', $name=null )
+    {
+        $cena = $this->decompose( $cenaId );
+        $formName = $this->cena . '[' . implode( '][', $cena ) . "][{$type}]";
+        if( $name ) $formName .= "[{$name}]";
+        return $formName;
+    }
+
+    /**
+     * extract data for Cena, or for CenaID.
+     * usage: extractData( $_POST, 'Cena.model.1.10' );
+     *
+     * @param array  $data
+     * @param string $cenaId
+     * @return array
+     */
+    public function extractData( $data, $cenaId=null )
+    {
+        // the data is not in Cena format.
+        // return the data as is.
+        if( !isset( $data[ $this->cena ] ) ) return $data;
+        // OK, got Cena formatted data.
+        $data = $data[ $this->cena ];
+        if( !$cenaId ) return $data;
+
+        // get data for a specific cenaID.
+        $cena = $this->decompose( $cenaId );
+        foreach( $cena as $item ) {
+            if( !isset( $data[ $item ] ) ) return array();
+            $data = $data[ $item ];
+        }
+        return $data;
+    }
+
+}
